7eeb553dfe17cf710ace05d56bb1d8da7a315ea3
Oleg Kasarin <>
2024-01-04 15:10:22 +0100

    Project setup

---
+++ WorldOfPAYBACK/WorldOfPAYBACK/WorldOfPAYBACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPAYBACKApp.swift
+//  WorldOfPAYBACK
+//
+//  Created by Oleg Kasarin on 04/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPAYBACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
