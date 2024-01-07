1fd73f826f6c35f59e9446b63cb11f0393556a35
Oleg Kasarin <>
2024-01-07 17:50:58 +0100

    Update view model

--- WorldOfPAYBACK/WorldOfPAYBACK/Modules/TransactionsList/TransactionsListViewModel.swift
+++ WorldOfPAYBACK/WorldOfPAYBACK/Modules/TransactionsList/TransactionsListViewModel.swift
@@ -9,8 +9,21 @@ import Foundation
 
 final class TransactionsListViewModel: ObservableObject {
     @Published var transactions: [Transaction] = []
+    @Published var selectedCategory: TransactionCategory = .all
+    
+    @Published var isLoading = false
+    @Published var isError = false
     
     private let transactionsService: TransactionsServiceProtocol
+    
+    var transactionsToDisplay: [Transaction] {
+        switch selectedCategory {
+        case .all:
+            return transactions
+        case .first, .second:
+            return transactions.filter { $0.category == selectedCategory.categoryNumber }
+        }
+    }
     
     init(transactionsService: TransactionsServiceProtocol) {
         self.transactionsService = transactionsService
@@ -18,10 +31,21 @@ final class TransactionsListViewModel: ObservableObject {
     
     @MainActor
     func fetch() async throws {
+        isLoading = true
         do {
-            transactions = try await transactionsService.fetchMocked()
+            let transactions = try await transactionsService.fetchMocked() 
+            let sortedTransactions = transactions.sorted(by: { $0.bookingDate > $1.bookingDate })
+            
+            isLoading = false
+            self.transactions = sortedTransactions
         } catch {
-            transactions = []
+            isLoading = false
+            isError = true
+//            transactions = []
         }
+    }
+    
+    func updateSelectedCategory(_ category: TransactionCategory) {
+        selectedCategory = category
     }
 }
